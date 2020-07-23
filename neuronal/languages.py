c080f74744c56de80042b02c3332e8031d13147d
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-07-22 21:59:12 -0400

    idioma Lain AI

---
+++ neuronal/languages.py
@@ -0,0 +1,12 @@
+langs={
+	"01":"binario",
+  "es-VE":"Spanish - Venezuela",
+  "ru":"Russian",
+	"uk":"Ukrainian",
+  "chi":"creahachipne",
+  "ja":"Japanese",
+  "zh":"Chinese ",
+  "ar":"Arabic",
+  "en":"English",
+  
+}
